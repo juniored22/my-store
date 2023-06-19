9c4a651d32265753a84c18a3378535753686d3eb
Edgard Junior <edgard.junior.dev@gmail.com>
2023-06-19 17:50:29 -0300

    page login

---
+++ App/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'pages', 'login.html'));
+});
+  
+router.get(["/", "/:name"], (req, res) => {
+    const greeting = "<h1>Hello From Node on Fly test!</h1>";
+    const name = req.params["name"];
+    if (name) {
+      res.send(greeting + "</br>and hello to " + name);
+    } else {
+      res.send(greeting);
+    }
+});
+  
+module.exports = router;
---
+++ App/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require('path');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Importar as rotas do arquivo separado
+app.use('/', routes);
+
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
